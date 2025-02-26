ec11fc0eee8f870794f048466cbe172212a34dd4
ggalanteCode <ggalante3@gmail.com>
2025-02-26 22:48:59 +0100

    Initial commit

---
+++ src/test/java/com/ggalantecode/musiccatalog/MusicCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggalantecode.musiccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
